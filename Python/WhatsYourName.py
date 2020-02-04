8c4c0505c3ed14d5781f2f53b5e8a74f8d450796
Pandu Cahyo Pangestu <pandusudo@gmail.com>
2020-02-04 12:12:36 +0700

    Update WhatsYourName.py

--- Python/WhatsYourName.py
+++ Python/WhatsYourName.py
@@ -1,2 +1,10 @@
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+# 
+# Hello firstname lastname! You just delved into python.
+# 
+# Input Format
+# 
+# The first line contains the first name, and the second line contains the last name.
+
 def print_full_name(a, b):
     print("Hello " + a + " " + b + "! You just delved into python.")
