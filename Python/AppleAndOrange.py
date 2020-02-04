eb7fd6f036478973a70bd160833758165926122c
Pandu Cahyo Pangestu <pandusudo@gmail.com>
2020-02-04 12:18:11 +0700

    Update AppleAndOrange.py

--- Python/AppleAndOrange.py
+++ Python/AppleAndOrange.py
@@ -1,3 +1,12 @@
+# Sam's house has an apple tree and an orange tree that yield an abundance of fruit. 
+# In the diagram below, the red region denotes his house, where S is the start point, and T is the endpoint. 
+# The apple tree is to the left of his house, and the orange tree is to its right. 
+# You can assume the trees are located on a single point, where the apple tree is at point A, and the orange tree is at point B.
+# 
+# When a fruit falls from its tree, it lands D units of distance from its tree of origin along the X-axis. 
+# A negative value of D means the fruit fell D units to the tree's left, and a positive value of D means it falls D units to the tree's right. 
+# Given the value of D for M apples and N oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range [s,t])?
+
 #!/bin/python3
 
 import math
